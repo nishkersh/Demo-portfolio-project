b5ea1cb8fd7169a239c3e138c5a569ae21657ea8
Reek Chatterjee <reekc11@gmail.com>
2022-08-19 13:07:12 +0530

    All files at initial stage

---
+++ reek.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){ 
+    $(window).scroll(function(){ 
+    // sticky navbar on scroll script
+    if(this.scrollY > 20){ 
+    $('.navbar').addClass("sticky"); 
+    }else{ 
+    $('.navbar').removeClass("sticky"); 
+    } 
+    
+    }); 
+    // toggle menu/navbar script
+    $('.menu-btn').click(function(){ 
+    $('.navbar .menu').toggleClass("active"); 
+    $('.menu-btn i').toggleClass("active"); 
+    }); 
+    // typing text animation script
+    var typed = new Typed(".typing", { 
+    strings: [ "Cryptography", "Cyber Security", "Devops", "Computer Networking" ], 
+    typeSpeed: 100, 
+    backSpeed: 60, 
+    loop: true
+    }); 
+   });
